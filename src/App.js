fa4139f2b9bf5735eef7da2ce7093f5a0f710216
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-14 12:07:02 -0400

    Implement a style div, Wrapper. Create Education component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { darkTheme, lightTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
 import Hero from "./components/HeroSection";
 import Skills from "./components/Skills";
+import Education from "./components/Education";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -12,13 +13,31 @@ const Body = styled.div`
   overflow-x: hidden;
 `;
 
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Navbar />
       <Body>
         <Hero />
-        <Skills />
+        <Wrapper>
+          <Skills />
+          <Education />
+        </Wrapper>
       </Body>
     </ThemeProvider>
   );
