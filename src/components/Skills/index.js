b027b3d1d5293accf1e24233a6002e3e4638357a
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-14 16:07:09 -0400

    Setup Skills component - pseudo code, start styling the component

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,5 +1,35 @@
-import React from "react";
+import styled from "styled-components";
+import { skills } from "../../data/constants";
 
 export default function Skills() {
-  return <div>Skills</div>;
+  return (
+    <Container id={"skills"}>
+      <Wrapper>
+        <Title>Skills</Title>
+        <Description>
+          Here are some of my skills on which I have been working on for the
+          past year.
+        </Description>
+        <SkillsContainer>
+          {skills.map((item) => {
+            return (
+              <Skill>
+                <SkillTitle>{item.title}</SkillTitle>
+                <SkillList>
+                  {item.skills.map((skill) => {
+                    return (
+                      <SkillItem>
+                        <SkillImage src={skill.icon} />
+                        <SkillName>{skill.name}</SkillName>
+                      </SkillItem>
+                    );
+                  })}
+                </SkillList>
+              </Skill>
+            );
+          })}
+        </SkillsContainer>
+      </Wrapper>
+    </Container>
+  );
 }
