d989f0e072b806c0cadb7febc98af1b632bac8c7
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-14 14:18:33 -0400

    Implement styled css for hero component

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,4 +1,91 @@
 import styled from "styled-components";
+import { Bio } from "../../data/constants";
+import { Title } from "@mui/icons-material";
+
+const HeroContainer = styled.div`
+  background-color: ${({ theme }) => theme.card_light};
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80 30px;
+  @media screen and (max-width: 960px) {
+    padding: 66px 16px;
+  }
+  @media screen and (max-width: 640px) {
+    padding: 32px 16px;
+  }
+  z-index: 1;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+const HeroBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 50%;
+  right: 0;
+  bottom: 0;
+  left: 50%;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  padding: 0 30px;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+  @media screen and (max-width: 960px) {
+    padding: 0 0px;
+    justify-content: center;
+  }
+`;
+
+const HeroInnerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+
+  @media screen and (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const HeroLeftContainer = styled.div`
+  width: 100%;
+  order: 1;
+  @media screen and (max-width: 960px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  @media screen and (max-width: 640px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+const HeroRightContainer = styled.div`
+  width: 100%;
+  order: 2;
+  display: flex;
+  justify-content: end;
+  gap: 12px;
+  @media screen and (max-width: 960px) {
+    order: 1;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 80px;
+  }
+  @media screen and (max-width: 640px) {
+    order: 1;
+    margin-bottom: 30px;
+  }
+`;
 
 export default function Hero() {
   return (
@@ -6,7 +93,12 @@ export default function Hero() {
       <HeroContainer>
         <HeroBg></HeroBg>
         <HeroInnerContainer>
-          <HeroLeftContainer></HeroLeftContainer>
+          <HeroLeftContainer>
+            <Title>
+              Hi, I am <br />
+              {Bio.name}
+            </Title>
+          </HeroLeftContainer>
           <HeroRightContainer></HeroRightContainer>
         </HeroInnerContainer>
       </HeroContainer>
