85eae7a8cf7f50091f3002aa1bae2b56272d4bb3
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-14 14:03:26 -0400

    Implement Navbar - Styled css, responsiveness

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/HeroSection";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -31,14 +32,16 @@ const Wrapper = styled.div`
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      <Navbar />
-      <Body>
-        <Hero />
-        <Wrapper>
-          <Skills />
-          <Education />
-        </Wrapper>
-      </Body>
+      <Router>
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+        </Body>
+      </Router>
     </ThemeProvider>
   );
 }
