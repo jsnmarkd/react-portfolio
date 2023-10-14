2aabd79f9d3c6e27e2952738bbbddd702a5ae594
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-14 11:13:02 -0400

    Implement theme, and create components for each section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import Hero from "./components/HeroSection";
+import Skills from "./components/Skills";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
-  return <div className="App">Portfolio</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+      <Body>
+        <Hero />
+        <Skills />
+      </Body>
+    </ThemeProvider>
+  );
 }
 
 export default App;
