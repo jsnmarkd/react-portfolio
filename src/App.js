59ad425eb51371fae6391ca618dd07272d33f2a4
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-16 14:53:30 -0400

    Implement Contact form - EmailJS

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -44,6 +45,7 @@ function App() {
           <Projects />
           <Wrapper>
             <Education />
+            <Contact />
           </Wrapper>
         </Body>
       </Router>
