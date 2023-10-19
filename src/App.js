2b84feb0703737761062b8a69444d7efd3cfa5d4
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-19 11:08:37 -0400

    Refactor Components - Hero, Footer, Experience, Education

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import styled, { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
 import Hero from "./components/HeroSection";
@@ -12,27 +12,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ProjectModal from "./components/Modal/ProjectModal";
-
-const Body = styled.div`
-  background-color: ${({ theme }) => theme.bg};
-  width: 100%;
-  overflow-x: hidden;
-`;
-
-const Wrapper = styled.div`
-  background: linear-gradient(
-      38.73deg,
-      rgba(204, 155, 187, 0.15) 0%,
-      rgba(201, 32, 184, 0) 50%
-    ),
-    linear-gradient(
-      141.27deg,
-      rgba(0, 70, 209, 0) 50%,
-      rgba(0, 70, 209, 0.15) 100%
-    );
-  width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
-`;
+import { Body, Wrapper } from "./AppStyle";
 
 function App() {
   const [openModal, setOpenModal] = useState({ state: false, project: null });
