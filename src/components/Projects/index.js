bf5dc28f8d8396ad6c7236a184202accbf5a2563
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-19 10:06:23 -0400

    Refactor - Project Component and Project Modal

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,104 +1,16 @@
 import { useState } from "react";
-import styled from "styled-components";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Description,
+  ToggleButton,
+  ToggleGroup,
+  Divider,
+  CardContainer,
+} from "./ProjectStyle";
 import ProjectCard from "../Cards/ProjectCard";
 import { projects } from "../../data/constants";
-
-const Container = styled.div`
-  background: linear-gradient(
-    343.07deg,
-    rgba(132, 59, 206, 0.06) 5.71%,
-    rgba(132, 59, 206, 0) 64.83%
-  );
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  position: relative;
-  z-index: 1;
-  align-items: center;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 100% 98%, 0 100%);
-`;
-
-const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  max-width: 1350px;
-  padding: 10px 0px 100px 0;
-  gap: 12px;
-  @media (max-width: 960px) {
-    flex-direction: column;
-  }
-`;
-
-const Title = styled.div`
-  font-size: 42px;
-  text-align: center;
-  font-weight: 600;
-  margin-top: 20px;
-  color: ${({ theme }) => theme.primary};
-
-  @media screen and (max-width: 768px) {
-    margin-top: 12px;
-    font-size: 32px;
-  }
-`;
-
-const Description = styled.div`
-  font-size: 18px;
-  max-width: 600px;
-  text-align: center;
-  color: ${({ theme }) => theme.text_secondary};
-  @media screen and (max-width: 768px) {
-    font-size: 16px;
-  }
-`;
-
-const ToggleGroup = styled.div`
-  display: flex;
-  border: 1.5px solid ${({ theme }) => theme.primary};
-  color: ${({ theme }) => theme.primary};
-  font-size: 16px;
-  border-radius: 12px;
-  font-weight: 500;
-  margin-top: 22px 0px;
-  @media screen and (max-width: 768px) {
-    font-size: 12px;
-  }
-`;
-
-const ToggleButton = styled.div`
-  padding: 8px 18px;
-  border-radius: 6px;
-  cursor: pointer;
-  ${({ active, theme }) =>
-    active &&
-    `
-    background: ${theme.primary + 20};
-    `}
-  &:hover {
-    background: ${({ theme }) => theme.primary + 8};
-  }
-  @media screen (max-width: 768px) {
-    padding: 6px 8px;
-    border-radius: 4px;
-  }
-`;
-
-const Divider = styled.div`
-  width: 1.5px;
-  background: ${({ theme }) => theme.primary};
-`;
-
-const CardContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 28px;
-`;
 
 export default function Projects({ openModal, setOpenModal }) {
   const [toggle, setToggle] = useState("all");
