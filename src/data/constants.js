8214c49519bca2aa2da18c9fe3730a5a8adcf301
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-16 19:54:35 -0500

    Update resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I'm a dedicated and adaptable person who's constantly seeking new opportunities. I have a strong enthusiasm for acquiring knowledge and am committed to producing excellent outcomes. My optimistic outlook and willingness to embrace growth enable me to make a valuable impact and accomplish significant achievements.",
   github: "https://github.com/jsnmarkd/",
   resume:
-    "https://drive.google.com/file/d/1zRm8q-rwtXpanw6pYKacyDEBSLbPG0y-/view?usp=sharing",
+    "https://drive.google.com/file/d/1Yd8UQBp_tSi3IGT7FocO7YD3tEV7LeMp/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/j-divinagracia/",
   insta: "https://www.instagram.com/jasondjsix/",
 };
